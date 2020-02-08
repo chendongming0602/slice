ec42f0661ab6e6df709aaa2d260bf10428418cce
chendongming <chendm0602@163.com>
2020-02-08 17:09:28 +0800

    tab完成

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,23 @@
+//index.js
+const APP = getApp()
+
+Page({
+  data: {
+    tabCount:0,//选择的tab
+    tabIf:[false,false,false],
+  },
+  tabIdex(e){//tab事件
+    this.setData({
+      tabCount:e.detail,
+      [`tabIf[${e.detail}]`]:true
+    });
+
+  },
+  onLoad: function() {
+    this.setData({
+      [`tabIf[${this.data.tabCount}]`]: true
+    });
+  },
+
+
+})
